61667b2b0fd92047464ce2fc1ebd5a8e5df76c53
Cj-bc <cj-bc@y-modify.org>
2018-12-25 23:39:43 +0900

    Add: head formula

---
+++ sonic-pi-tool.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Sonicpitool < Formula
+  homepage "https://github.com/lpil/sonic-pi-tool"
+  head "https://github.com/lpil/sonic-pi-tool"
+  sha256 "8424409d1e1162ec0c6c81e380018742f8c140efa1481d1f54732118fdcb45e0"
+
+  depends_on "rust"
+
+  def install
+    system "cargo install --git https://github.com/lpil/sonic-pi-tool/ --force --root " prefix
+  end
+
+end
